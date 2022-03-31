d65b1b2efa7a7c180ad819dd2fc3c3594a6ce6a6
NaniMadhu94 <102646505+NaniMadhu94@users.noreply.github.com>
2022-03-31 11:21:58 +0530

    java scripting tasks
    20 codes

---
+++ array-filtering.js
@@ -0,0 +1,19 @@
+let numbers= [1,2,3,4,5,6,7,8,9,10];
+
+let a=[];
+
+for(let i=0; ;i++)
+{
+
+    if(numbers[i]%2==0)
+    {
+      a.push(numbers[i]);
+    }
+}
+
+console.log(a);
+
+
+
+
+
---
+++ duplicate.js
@@ -0,0 +1,4 @@
+const array = [1,2,2,3,4,5,6,7,8,93,21,5,4,1,2,]
+let uniqueArray = [new Set(array)]
+let b= uniqueArray.sort();
+console.log(b);
